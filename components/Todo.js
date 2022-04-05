0dcdfdeadd8c8195193f549bdeb5cc3d8116b543
maribelcuales <maribelcuales@gmail.com>
2022-04-05 22:21:10 +0800

    Added initial code for Todo component.

---
+++ components/Todo.js
@@ -0,0 +1,8 @@
+// App Layout
+import { React, useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
+import TodoItem from "./TodoItem";
+
+function Todo() {
+  return <div></div>;
+}
