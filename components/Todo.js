13928b9ea4a9d44db07b7888ff69d63c0e07758a
maribelcuales <maribelcuales@gmail.com>
2022-04-10 22:05:44 +0800

    Set state and added event handler functions.

--- components/Todo.js
+++ components/Todo.js
@@ -4,5 +4,13 @@ import styles from "../styles/Home.module.css";
 import TodoItem from "./TodoItem";
 
 function Todo() {
+  const [newTodo, setNewTodo] = useState("");
+  const handleInput = (e) => {
+    setNewTodo(e.target.value); 
+  };
+  const handleSubmit = (e) => {
+    console.log(newTodo); 
+  }
+
   return <div></div>;
 }
