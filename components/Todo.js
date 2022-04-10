d61ecb01da4c15e83e754ac4698d687d369ad2ca
maribelcuales <maribelcuales@gmail.com>
2022-04-10 22:24:20 +0800

    Render Todo App in Todo component.

--- components/Todo.js
+++ components/Todo.js
@@ -3,14 +3,34 @@ import { React, useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import TodoItem from "./TodoItem";
 
-function Todo() {
+export default function Todo() {
   const [newTodo, setNewTodo] = useState("");
   const handleInput = (e) => {
-    setNewTodo(e.target.value); 
+    setNewTodo(e.target.value);
   };
   const handleSubmit = (e) => {
-    console.log(newTodo); 
-  }
+    console.log(newTodo);
+  };
 
-  return <div></div>;
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <div>
+        <h3>Add New Todo</h3>
+        <div>
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => handleInput(e)}
+          ></input>
+          <button onClick={() => handleSubmit()}>
+            Add Todo
+          </button>
+        </div>
+      </div>
+      <div>
+        <TodoItem />
+      </div>
+    </div>
+  );
 }
