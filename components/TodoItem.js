834f3063f328d860293818601072d84e6b28b072
maribelcuales <maribelcuales@gmail.com>
2022-04-13 21:51:51 +0800

    Added initial code to TodoItem component.

---
+++ components/TodoItem.js
@@ -0,0 +1,9 @@
+import { defaultConfig } from 'next/dist/server/config-shared';
+import { React, useState} from 'react';
+import styles from '../../styles/Home.module.css';
+
+export default TodoItem() {
+  return(
+    <div></div>
+  )
+}
