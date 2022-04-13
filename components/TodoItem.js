b8c09545a7baa3ce41622eaac8a6e853e404f8be
maribelcuales <maribelcuales@gmail.com>
2022-04-13 22:01:35 +0800

    Added state for todo item.

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -3,7 +3,15 @@ import { React, useState} from 'react';
 import styles from '../../styles/Home.module.css';
 
 export default TodoItem() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleDelete = () => {};
+
+  const handleCheck = async () => {};
+
   return(
-    <div></div>
+    <div>
+    
+    </div>
   )
 }
