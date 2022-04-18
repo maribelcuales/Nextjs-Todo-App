b1faa2fac56aa3634d635b320c6f2d75bb6be436
maribelcuales <maribelcuales@gmail.com>
2022-04-18 21:33:59 +0800

    Pass in todo props in TodoItem component.

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -2,7 +2,7 @@ import { defaultConfig } from 'next/dist/server/config-shared';
 import { React, useState} from 'react';
 import styles from '../styles/Home.module.css';
 
-export default function TodoItem() {
+export default function TodoItem({ todo }) {
   const [isChecked, setIsChecked] = useState(false);
 
   const handleDelete = () => {};
@@ -12,7 +12,7 @@ export default function TodoItem() {
   return(
     <div>
       <span className={styles.eachTodo}>
-        <p className={styles.text}>Eat Breakfast</p>
+        <p className={styles.text}>{todo.data.task}</p>
         <div>
           <input
             type="checkbox"
