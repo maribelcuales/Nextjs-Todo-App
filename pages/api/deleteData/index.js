00de324f08923c1ff547cb3ca9f7f01cc1d285a6
maribelcuales <maribelcuales@gmail.com>
2022-04-25 22:25:25 +0800

    Setup the deleteData functionality.

---
+++ pages/api/deleteData/index.js
@@ -0,0 +1,17 @@
+const { Ref } = require("faunadb");
+const faunadb = require("faunadb");
+const secret = process.env.DB_SECRET_KEY;
+const q = faunadb.query;
+const client = new faunadb.Client({ secret });
+
+module.exports = async (req, res) => {
+  const id = req.body.id;
+  try {
+    const dbs = await client.query(
+      q.Delete(q.Ref(q.Collection("todos"), id))
+    );
+    res.status(200).json(dbs.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
